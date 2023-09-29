2778b0217adb355d9bdea29239917e4431484077
alejandro20042 <alejandrosillasalcantar.com@gmail.com>
2023-09-29 09:16:50 -0700

    Agregar archivos de proyecto.

---
+++ Examen_ASA/Program.cs
@@ -0,0 +1,73 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_ASA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            //Console.WriteLine("Introduce el primer numero");
+            //int numero1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Introduce el segundo numero");
+            //int numero2 = Convert.ToInt32(Console.ReadLine());
+
+            //if (numero1 <= numero2)
+            //{
+            //    for (int x = numero1; x <= numero2; x++)
+            //    {
+            //        Console.WriteLine("la secuencia de los números es " + x);
+            //    }
+            //}
+            //else
+            //{
+            //    for (int x = numero1; x >= numero2; x--)
+            //    {
+            //        Console.WriteLine("la secuencia de los números es " + x);
+            //    }
+            //}
+
+
+        //    Console.WriteLine("Tabla de multiplicar del 1 al 10 ");
+        //    ImprimirTablaDeMultiplicar(1);
+
+        //}
+
+
+
+        //static void ImprimirTablaDeMultiplicar(int numero, int multiplicador = 1)
+        //{
+        //    if (multiplicador <= 10)
+        //    {
+        //        Console.WriteLine($"{numero} x {multiplicador} = {numero * multiplicador}");
+        //        ImprimirTablaDeMultiplicar(numero, multiplicador + 1);
+        //    }
+        //    else
+        //    {
+
+        //        if (numero < 10)
+        //        {
+        //            ImprimirTablaDeMultiplicar(numero + 1);
+        //        }
+        //    }
+        //}
+
+
+
+
+
+
+
+    }
+
+    
+}
+
